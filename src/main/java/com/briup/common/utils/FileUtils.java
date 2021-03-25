079d5906e8d0f46962fd712a5a3b16c87f744e76
Yong-g <78772185+Yong-g@users.noreply.github.com>
2021-03-25 23:37:06 +0800

    file upload

--- src/main/java/com/briup/common/utils/FileUtils.java
+++ src/main/java/com/briup/common/utils/FileUtils.java
@@ -1,2 +1,61 @@
-package com.briup.common.utils;public class FileUtils {
+package com.briup.common.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class FileUtils {
+
+    private static File file;
+
+
+/*
+    static {
+        if (null == file){
+            String filePath = System.getProperty("user.dir");
+            file = new File(filePath+"\\photo");
+            if (!file.exists()){
+                file.mkdirs();
+            }
+        }
+    }
+*/
+
+    public static void readAndWrite(InputStream in, OutputStream out) throws IOException {
+        int len = 0;
+        byte[] b = new byte[1024];
+        while ((len = in.read(b)) != -1 ){
+            out.write(b);
+        }
+        in.close();
+        out.flush();
+        out.close();
+    }
+
+    public FileUtils() {
+    }
+
+    public static File getFile() {
+        if (null == file){
+            String filePath = System.getProperty("user.dir");
+            file = new File(filePath+"\\photo");
+            if (!file.exists()){
+                file.mkdirs();
+            }
+        }
+        return file;
+    }
+
+    public static String getFilePath(){
+        if (null == file){
+            String filePath = System.getProperty("user.dir");
+            file = new File(filePath+"\\photo");
+            if (!file.exists()){
+                file.mkdirs();
+            }
+        }
+        return file.getPath();
+    }
 }
--- src/main/java/com/briup/controller/ProductController.java
+++ src/main/java/com/briup/controller/ProductController.java
@@ -2,13 +2,20 @@ package com.briup.controller;
 
 import com.briup.bean.product.Product;
 import com.briup.common.respose.SimpleRespose;
+import com.briup.common.utils.FileUtils;
 import com.briup.dao.ProductDao;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -44,4 +51,26 @@ public class ProductController {
         return new SimpleRespose(productDao.findById(id).isPresent() ? productDao.findById(id).get() : null,null, "0");
     }
 
+    @PostMapping("imageUpload")
+    @ResponseBody
+    public Object imageUpload(@RequestBody MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String fileName = originalFilename.substring(0, originalFilename.lastIndexOf("."));
+        String fileType = originalFilename.substring(originalFilename.lastIndexOf("."));
+        fileName = fileName.concat(String.valueOf(System.currentTimeMillis())).concat(fileType);
+        FileUtils.readAndWrite(file.getInputStream(),new FileOutputStream(new File(FileUtils.getFilePath().concat("\\"+fileName))));
+        return  new SimpleRespose(fileName,"上传成功","0");
+    }
+
+    @GetMapping("getImage")
+    @ResponseBody
+    public Object getImage(@RequestParam String file, HttpServletResponse response) throws IOException {
+        ServletOutputStream outputStream = response.getOutputStream();
+        File file1 = new File(FileUtils.getFilePath().concat("\\"+file));
+        if (file1.exists()){
+            FileUtils.readAndWrite(new FileInputStream(file1),outputStream);
+            return null;
+        }
+        return new SimpleRespose(null,"文件不存在","1");
+    }
 }
