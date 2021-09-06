bbfe794bff6c76ade00369280e3eabdbb71d3110
陈国庆 <cgq786153492@gmail.com>
2021-09-06 10:31:05 +0800

    fix bug: redis

--- src/main/java/com/briup/common/utils/RedisUtil.java
+++ src/main/java/com/briup/common/utils/RedisUtil.java
@@ -3,18 +3,33 @@ package com.briup.common.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+
+import java.net.URI;
+
 @Component
 public class RedisUtil {
 
-    private Logger logger = LoggerFactory.getLogger(RedisUtil.class);
+    private final Logger logger = LoggerFactory.getLogger(RedisUtil.class);
     private static Jedis jedis;
-    private static String HOST = "localhost";
+    private static String HOST = "ec2-3-88-74-91.compute-1.amazonaws.com";
+    private static int PORT = 25360;
 
     private void openConnect(){
-        Jedis jedis = new Jedis("ec2-3-88-74-91.compute-1.amazonaws.com");
+        URI uri = null;
+        try {
+            uri = new URI("redis://:p257e44183eec6c4afa627e187812eceb2efde632b874b6cfd4a5942e4f73a943@ec2-3-88-74-91.compute-1.amazonaws.com:25360");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (uri.equals(null)) {
+            throw new RuntimeException("URI创建失败");
+        }
+        HostAndPort hostAndPort = new HostAndPort(HOST, PORT);
+        Jedis jedis = new Jedis(uri);
         String ping = jedis.ping();
-        logger.info(ping);
+        logger.info("redis 连接测试: {}",ping);
     }
 
     public Jedis getConnect(){
